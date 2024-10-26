1ef20835c581c33b8a6f419f289896cd35a6e0a1
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-10-26 20:08:02 +0800

    Initial Commit

---
+++ SimpleFusionCam/SimpleFusionCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFusionCamApp.swift
+//  SimpleFusionCam
+//
+//  Created by Andriyanto Halim on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFusionCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleFusionCamTests/SimpleFusionCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFusionCamTests.swift
+//  SimpleFusionCamTests
+//
+//  Created by Andriyanto Halim on 26/10/24.
+//
+
+import Testing
+@testable import SimpleFusionCam
+
+struct SimpleFusionCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
